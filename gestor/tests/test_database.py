7d1b8c2c9fcaa951f095445db0e8ad80edb8522f
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-09-23 19:07:45 +0200

    cambios

--- gestor/tests/test_database.py
+++ gestor/tests/test_database.py
@@ -6,7 +6,7 @@ import unittest
 import database as db
 
 class TestDatabase(unittest.TestCase):
-    
+
     def setUp(self):
         db.Clientes.lista = [
             db.Cliente('15J', 'Marta', 'Pérez'),
