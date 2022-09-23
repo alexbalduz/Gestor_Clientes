af24858322a323041217f00fd8c674ee06a622f6
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-09-23 12:55:48 +0200

    subventana

--- gestor/ui.py
+++ gestor/ui.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 import database as db
+from tkinter.messagebox import askokcancel, WARNING
+
 
 class CenterWidgetMixin:
     def center(self):
@@ -55,3 +57,50 @@ class MainWindow(Tk, CenterWidgetMixin):
         Button(frame, text="Borrar", command=self.delete).grid(row=0, column=2)
 
         self.treeview = treeview
+
+    def delete(self):
+        cliente = self.treeview.focus()
+        if cliente:
+            campos = self.treeview.item(cliente, "values")
+            confirmar = askokcancel(
+                title="Confirmar borrado",
+                message=f"¿Borrar {campos[1]} {campos[2]}?",
+                icon=WARNING)
+            if confirmar:
+                self.treeview.delete(cliente)
+                db.Clientes.borrar(campos[0])
+
+class CreateClientWindow(Toplevel, CenterWidgetMixin):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title("Crear cliente")
+        self.build()
+        self.center()
+        self.transient(parent)
+        self.grab_set()
+
+    def build(self):
+        frame = Frame(self)
+        frame.pack(padx=20, pady=10)
+
+        Label(frame, text="DNI (2 ints y 1 upper char)").grid(row=0, column=0)
+        Label(frame, text="Nombre (de 2 a 30 chars)").grid(row=0, column=1)
+        Label(frame, text="Apellido (de 2 a 30 chars)").grid(row=0, column=2)
+
+        dni = Entry(frame)
+        dni.grid(row=1, column=0)
+        dni.bind("<KeyRelease>", lambda event: self.validate(event, 0))
+        nombre = Entry(frame)
+        nombre.grid(row=1, column=1)
+        nombre.bind("<KeyRelease>", lambda event: self.validate(event, 1))
+        apellido = Entry(frame)
+        apellido.grid(row=1, column=2)
+        apellido.bind("<KeyRelease>", lambda event: self.validate(event, 2))
+
+        frame = Frame(self)
+        frame.pack(pady=10)
+
+        crear = Button(frame, text="Crear", command=self.create_client)
+        crear.configure(state=DISABLED)
+        crear.grid(row=0, column=0)
+        Button(frame, text="Cancelar", command=self.close).grid(row=0, column=1)
