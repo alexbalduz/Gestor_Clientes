2ea0c983192a71c4195bd6e042c35c7841a5b255
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-09-23 12:51:25 +0200

    main window

--- gestor/ui.py
+++ gestor/ui.py
@@ -1,17 +1,57 @@
 from tkinter import *
-class MainWindow(Tk):
+from tkinter import ttk
+import database as db
+
+class CenterWidgetMixin:
+    def center(self):
+        self.update()
+        w = self.winfo_width()
+        h = self.winfo_height()
+        ws = self.winfo_screenwidth()
+        hs = self.winfo_screenheight()
+        x = int(ws/2 - w/2)
+        y = int(hs/2 - h/2)
+        self.geometry(f"{w}x{h}+{x}+{y}")
+
+class MainWindow(Tk, CenterWidgetMixin):
     def __init__(self):
         super().__init__()
-        self.title('Gestor de clientes')
+        self.title("Gestor de clientes")
         self.build()
+        self.center()
 
     def build(self):
-        button = Button(self.root, text="Hola", command=self.hola)
-        button.pack()
+        frame = Frame(self)
+        frame.pack()
 
-    def hola(self):
-        print("¡Hola mundo!")
+        treeview = ttk.Treeview(frame)
+        treeview['columns'] = ('DNI', 'Nombre', 'Apellido')
 
-if __name__ == "__main__":
-    app = MainWindow()
-    app.mainloop()
+        treeview.column("#0", width=0, stretch=NO)
+        treeview.column("DNI", anchor=CENTER)
+        treeview.column("Nombre", anchor=CENTER)
+        treeview.column("Apellido", anchor=CENTER)
+
+        treeview.heading("DNI", text="DNI", anchor=CENTER)
+        treeview.heading("Nombre", text="Nombre", anchor=CENTER)
+        treeview.heading("Apellido", text="Apellido", anchor=CENTER)
+
+        scrollbar = Scrollbar(frame)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        treeview['yscrollcommand'] = scrollbar.set
+
+        for cliente in db.Clientes.lista:
+            treeview.insert(
+                parent='', index='end', iid=cliente.dni,
+                values=(cliente.dni, cliente.nombre, cliente.apellido))
+
+        treeview.pack()
+
+        frame = Frame(self)
+        frame.pack(pady=20)
+
+        Button(frame, text="Crear", command=self.create).grid(row=0, column=0)
+        Button(frame, text="Modificar", command=self.edit).grid(row=0, column=1)
+        Button(frame, text="Borrar", command=self.delete).grid(row=0, column=2)
+
+        self.treeview = treeview
